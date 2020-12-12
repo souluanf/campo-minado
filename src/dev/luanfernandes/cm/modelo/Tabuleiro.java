b7fc4c468efba53477922ea9cdf54e6898c1041b
Luan Fernandes <hello@luanfernandes.dev>
2020-12-12 05:27:23 -0300

    Classe Tabuleiro: gerarCampos(), associarVizinhos()

---
+++ src/dev/luanfernandes/cm/modelo/Tabuleiro.java
@@ -0,0 +1,45 @@
+package dev.luanfernandes.cm.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tabuleiro {
+	private int linhas;
+	private int colunas;
+	private int minas;
+
+	private final List<Campo> campos = new ArrayList<>();
+
+	public Tabuleiro(int linhas, int colunas, int minas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		this.minas = minas;
+
+		gerarCampos();
+		associasVizinhos();
+		sortearMinas();
+
+	}
+
+	private void gerarCampos() {
+		for (int linha = 0; linha < linhas; linha++) {
+			for (int coluna = 0; coluna < colunas; coluna++) {
+				campos.add(new Campo(linha, coluna));
+			}
+		}
+
+	}
+
+	private void associasVizinhos() {
+		for (Campo c1 : campos) {
+			for (Campo c2 : campos) {
+				c1.adicionarVizinho(c2);
+			}
+		}
+	}
+
+	private void sortearMinas() {
+
+	}
+
+}
