208dd1fc49b61c1f696c30d46b1e6f3c5d9d6723
Luan Fernandes <hello@luanfernandes.dev>
2020-12-12 03:37:22 -0300

    Classe Campo: Adicionar Vizinhos

--- src/dev/luanfernandes/cm/modelo/Campo.java
+++ src/dev/luanfernandes/cm/modelo/Campo.java
@@ -18,5 +18,25 @@ public class Campo {
 		this.linha = linha;
 		this.coluna = coluna;
 	}
+	
+	boolean adicionarVizinho(Campo vizinho) {
+		boolean linhaDiferente = linha != vizinho.linha;
+		boolean colunaDiferente = linha != vizinho.coluna;
+		boolean diagonal = linhaDiferente && colunaDiferente;
+		
+		int deltaLinha = Math.abs(linha - vizinho.linha);
+		int deltaColuna = Math.abs(coluna - vizinho.coluna);
+		int deltaGeral = deltaColuna + deltaLinha;
+		
+		if (deltaGeral ==1 && !diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+		}else if(deltaGeral ==2 && diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+		}else {
+			return false;
+		}
+	}
 
 }
