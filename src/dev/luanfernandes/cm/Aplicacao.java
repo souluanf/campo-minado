15c611c49365306d33d7c86f37d32eb938557ac3
Luan Fernandes <hello@luanfernandes.dev>
2020-12-12 06:24:50 -0300

    Tabuleiro Console

--- src/dev/luanfernandes/cm/Aplicacao.java
+++ src/dev/luanfernandes/cm/Aplicacao.java
@@ -1,17 +1,14 @@
 package dev.luanfernandes.cm;
 
 import dev.luanfernandes.cm.modelo.Tabuleiro;
+import dev.luanfernandes.cm.visao.TabuleiroConsole;
 
 public class Aplicacao {
 	
 	public static void main(String[] args) {
 		
 		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
-		tabuleiro.abrir(3, 3);
-		tabuleiro.alternarMarcacao(4,4);
-		tabuleiro.alternarMarcacao(4,5);
-		
-		System.out.println(tabuleiro);
+		new TabuleiroConsole(tabuleiro);
 	}
 
 }
