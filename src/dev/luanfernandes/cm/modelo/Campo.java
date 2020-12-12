5dfafad2e213f5847ef63ca7457f9db6c3ecbee2
Luan Fernandes <hello@luanfernandes.dev>
2020-12-12 05:14:10 -0300

    Classe Campo: toString()

--- src/dev/luanfernandes/cm/modelo/Campo.java
+++ src/dev/luanfernandes/cm/modelo/Campo.java
@@ -94,22 +94,37 @@ public class Campo {
 	public int getColuna() {
 		return coluna;
 	}
-	
+
 	boolean objetivoAlcancado() {
 		boolean desvendado = !minado && aberto;
 		boolean protegido = minado && marcado;
-		
+
 		return desvendado || protegido;
 	}
-	
+
 	long minasNaVizinhanca() {
 		return vizinhos.stream().filter(v -> v.minado).count();
 	}
-	
+
 	void reiniciar() {
 		aberto = false;
 		minado = false;
 		marcado = false;
+	}
+
+	@Override
+	public String toString() {
+		if (marcado) {
+			return "x";
+		} else if (aberto && minado) {
+			return "*";
+		} else if (aberto && minasNaVizinhanca() > 0) {
+			return Long.toString(minasNaVizinhanca());
+		} else if (aberto) {
+			return " ";
+		} else {
+			return "?";
+		}
 	}
 
 }
