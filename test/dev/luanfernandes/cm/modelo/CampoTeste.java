10eb2e0eb31ccbce97531933511b8b4f47d1d132
Luan Fernandes <hello@luanfernandes.dev>
2020-12-12 04:37:17 -0300

    Classe CampoTeste: testeAbrirComVizinhos2()

--- test/dev/luanfernandes/cm/modelo/CampoTeste.java
+++ test/dev/luanfernandes/cm/modelo/CampoTeste.java
@@ -65,43 +65,43 @@ public class CampoTeste {
 
 		assertFalse(resultado);
 	}
-	
+
 	@Test
 	void testeValorPadraoAtributoMarcado() {
 		assertFalse(campo.isMarcado());
 	}
-	
+
 	@Test
 	void alternarMarcacao() {
 		campo.alternarMarcacao();
 		assertTrue(campo.isMarcado());
 	}
-	
+
 	@Test
 	void alternarMarcacaoDuasChamadas() {
 		campo.alternarMarcacao();
 		campo.alternarMarcacao();
 		assertFalse(campo.isMarcado());
 	}
-	
+
 	@Test
 	void testeAbrirNaoMinadoNaoMarcado() {
 		assertTrue(campo.abrir());
 	}
-	
+
 	@Test
 	void testeAbrirNaoMinadoMarcado() {
 		campo.alternarMarcacao();
 		assertFalse(campo.abrir());
 	}
-	
+
 	@Test
 	void testeAbrirMinadoMarcado() {
 		campo.alternarMarcacao();
 		campo.minar();
 		assertFalse(campo.abrir());
 	}
-	
+
 	@Test
 	void testeAbrirMinadoNaoMarcado() {
 		campo.minar();
@@ -109,17 +109,33 @@ public class CampoTeste {
 			campo.abrir();
 		});
 	}
-	
+
 	@Test
 	void testeAbrirComVizinhos1() {
 		Campo campo11 = new Campo(1, 1);
 		Campo campo22 = new Campo(2, 2);
 		campo22.adicionarVizinho(campo11);
-		
+
 		campo.adicionarVizinho(campo22);
 		campo.abrir();
 
 		assertTrue(campo22.isAberto() && campo11.isAberto());
+	}
+
+	@Test
+	void testeAbrirComVizinhos2() {
+		Campo campo11 = new Campo(1, 1);
+		Campo campo12 = new Campo(1, 1);
+		campo12.minar();
+
+		Campo campo22 = new Campo(2, 2);
+		campo22.adicionarVizinho(campo11);
+		campo22.adicionarVizinho(campo12);
+
+		campo.adicionarVizinho(campo22);
+		campo.abrir();
+
+		assertTrue(campo22.isAberto() && campo11.isFechado());
 	}
 
 }
