ef02871ee4116931641d50932e2bc639d52704c4
Luan Fernandes <hello@luanfernandes.dev>
2020-12-12 04:00:37 -0300

    Classe Campo: isMarcado()

--- src/dev/luanfernandes/cm/modelo/Campo.java
+++ src/dev/luanfernandes/cm/modelo/Campo.java
@@ -68,5 +68,12 @@ public class Campo {
 	boolean vizinhancaSegura() {
 		return vizinhos.stream().noneMatch(v -> v.minado);
 	}
+	
+	public boolean isMarcado() {
+		return marcado;
+	}
+	
+	
+	
 
 }
